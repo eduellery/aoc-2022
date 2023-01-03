e9fbf40ec2ad93fd3ef638aab243413ba707fac8
Eduardo Ellery <eduellery@gmail.com>
2023-01-03 20:23:04 +0100

    2022 - Day 01 - Part 2

--- src/aoc/day01.ts
+++ src/aoc/day01.ts
@@ -3,3 +3,10 @@ export function part1(entries: number[][]): number {
         return current.reduce<number>((sum, calories) => { return sum + calories }, 0)
     }))
 }
+
+export function part2(entries: number[][]): number {
+    var ordered = entries.map(current => {
+        return current.reduce<number>((sum, calories) => { return sum + calories }, 0)
+    }).sort((a, b) => b - a);
+    return ordered.slice(0, 3).reduce<number>((sum, calories) => { return sum + calories }, 0)
+}
--- test/day01.test.ts
+++ test/day01.test.ts
@@ -1,23 +1,31 @@
-import { part1 } from "../src/aoc/day01";
-
+import { part1, part2 } from "../src/aoc/day01";
 import fs from 'fs';
-
-describe("Day 01", () => {
-    it("test part 1", () => {
-        const input = fs.readFileSync(__dirname + '/day01.test', 'utf8');
-        const entries = generateEntries(input)
-        const result = part1(entries);
-        expect(result).toEqual(24000);
-    });
-
-    it("solve part 1", () => {
-        const input = fs.readFileSync(__dirname + '/day01.in', 'utf8');
-        const entries = generateEntries(input)
-        const result = part1(entries);
-        expect(result).toEqual(71502);
-    });
-});
 
 function generateEntries(input: any) {
     return input.split('\n\n').map((entry: string) => entry.split('\n').map(Number)) as Array<number[]>;
 }
+
+const test = generateEntries(fs.readFileSync(__dirname + '/day01.test', 'utf8'));
+const input = generateEntries(fs.readFileSync(__dirname + '/day01.in', 'utf8'));
+
+describe("Day 01", () => {
+    it("test part 1", () => {
+        const result = part1(test);
+        expect(result).toEqual(24000);
+    });
+
+    it("solve part 1", () => {
+        const result = part1(input);
+        expect(result).toEqual(71502);
+    });
+
+    it("test part 2", () => {
+        const result = part2(test);
+        expect(result).toEqual(45000);
+    });
+
+    it("solve part 2", () => {
+        const result = part2(input);
+        expect(result).toEqual(208191);
+    });
+});
