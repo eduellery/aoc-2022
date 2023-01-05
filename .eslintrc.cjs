d879b95155199b0a1030add096072a750823e9f1
Eduardo Ellery <eduellery@gmail.com>
2023-01-05 19:22:31 +0100

    Refactor setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true
+    },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
--- test/day01.test.ts
+++ test/day01.test.ts
@@ -1,4 +1,4 @@
-import { part1, part2 } from "../src/aoc/day01";
+import {part1, part2} from "../src/aoc/day01";
 import fs from 'fs';
 
 function generateEntries(input: string) {
@@ -9,23 +9,27 @@ const test = generateEntries(fs.readFileSync(__dirname + '/day01.test', 'utf8'))
 const input = generateEntries(fs.readFileSync(__dirname + '/day01.in', 'utf8'));
 
 describe("Day 01", () => {
-    it("test part 1", () => {
-        const result = part1(test);
-        expect(result).toEqual(24000);
+    describe("Part 1", () => {
+        it("Sample", () => {
+            const result = part1(test);
+            expect(result).toEqual(24000);
+        });
+
+        it("Input", () => {
+            const result = part1(input);
+            expect(result).toEqual(71502);
+        });
     });
 
-    it("solve part 1", () => {
-        const result = part1(input);
-        expect(result).toEqual(71502);
-    });
+    describe("Part 2", () => {
+        it("Sample", () => {
+            const result = part2(test);
+            expect(result).toEqual(45000);
+        });
 
-    it("test part 2", () => {
-        const result = part2(test);
-        expect(result).toEqual(45000);
-    });
-
-    it("solve part 2", () => {
-        const result = part2(input);
-        expect(result).toEqual(208191);
+        it("Solve", () => {
+            const result = part2(input);
+            expect(result).toEqual(208191);
+        })
     });
 });
