0d017b72fe52adc348521011c56c9c4d4c62f432
Eduardo Ellery <eduellery@gmail.com>
2023-12-09 08:42:16 +0100

    Day 2022 - Day 02 - Part 2

--- src/aoc/day02.ts
+++ src/aoc/day02.ts
@@ -1,20 +1,20 @@
-const value: { [key: string]: number } = {
-    'A X': 4,
-    'A Y': 8,
-    'A Z': 3,
-    'B X': 1,
-    'B Y': 5,
-    'B Z': 9,
-    'C X': 7,
-    'C Y': 2,
-    'C Z': 6,
-    else: 0
+const value: { [key: string]: [number, number] } = {
+    'A X': [4, 3],
+    'A Y': [8, 4],
+    'A Z': [3, 8],
+    'B X': [1, 1],
+    'B Y': [5, 5],
+    'B Z': [9, 9],
+    'C X': [7, 2],
+    'C Y': [2, 6],
+    'C Z': [6, 7],
+    else: [0, 0]
 };
 
 export function part1(entries: string[]): number {
-    return entries.reduce<number>((sum, entry) => { return sum + value[entry] }, 0)
+    return entries.reduce<number>((sum, entry) => { return sum + value[entry][0] }, 0)
 }
 
 export function part2(entries: string[]): number {
-    return entries.length
+    return entries.reduce<number>((sum, entry) => { return sum + value[entry][1] }, 0)
 }
--- test/day02.test.ts
+++ test/day02.test.ts
@@ -1,4 +1,4 @@
-import {part1, part2} from "../src/aoc/day02";
+import { part1, part2 } from "../src/aoc/day02";
 import fs from 'fs';
 
 function generateEntries(input: string) {
@@ -24,12 +24,12 @@ describe("Day 02", () => {
     describe("Part 2", () => {
         it("Sample", () => {
             const result = part2(test);
-            expect(result).toEqual(3);
+            expect(result).toEqual(12);
         });
 
         it("Solve", () => {
             const result = part2(input);
-            expect(result).toEqual(2500);
+            expect(result).toEqual(11373);
         })
     });
 });
